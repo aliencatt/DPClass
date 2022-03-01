97c7772f7fab96277d3e8fa6dc8a31ceafea3074
Asta <78469700+astacode@users.noreply.github.com>
2022-03-01 10:42:56 +0530

    second commit

--- src/DP/Factory/Test.java
+++
@@ -1,5 +0,0 @@
-package DP.Factory;
-
-public class Test {
-
-}
---
+++ src/DP/Lecture/Abstract_Factory/Test.java
@@ -0,0 +1,9 @@
+package DP.Lecture.Abstract_Factory;
+
+public class Test {
+
+  public static void main(String[] args) {
+
+  }
+
+}
